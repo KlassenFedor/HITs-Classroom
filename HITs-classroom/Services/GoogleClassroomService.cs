a79d328e88ff311f0ae0b38287e74926f712fdf5
Klassen Fedor <fyodor.klassen@yandex.ru>
2022-09-30 13:28:02 +0700

    New GCService version

--- HITs-classroom/Services/GoogleClassroomService.cs
+++ HITs-classroom/Services/GoogleClassroomService.cs
@@ -1,7 +1,11 @@
 ﻿using Google.Apis.Auth.OAuth2;
 using Google.Apis.Classroom.v1;
+using Google.Apis.IAMCredentials.v1;
 using Google.Apis.Services;
 using Google.Apis.Util.Store;
+using static Google.Apis.IAMCredentials.v1.ProjectsResource;
+using System.Diagnostics;
+using Google.Apis.IAMCredentials.v1.Data;
 
 namespace HITs_classroom.Services
 {
@@ -10,13 +14,13 @@ namespace HITs_classroom.Services
         static string[] Scopes = { 
             ClassroomService.Scope.ClassroomCourses,
             ClassroomService.Scope.ClassroomRosters,
-            ClassroomService.Scope.ClassroomProfileEmails,
-            "https://www.googleapis.com/auth/cloud-platform"
+            ClassroomService.Scope.ClassroomProfileEmails
         };
 
         public ClassroomService GetClassroomService(string relatedUser)
         {
-            UserCredential credential = MakeCredential(relatedUser);
+            string accessToken = GetAccessToken(relatedUser);
+            GoogleCredential credential = GoogleCredential.FromAccessToken(accessToken);
             ClassroomService classroomService = new ClassroomService(new BaseClientService.Initializer
             {
                 HttpClientInitializer = credential,
@@ -42,6 +46,47 @@ namespace HITs_classroom.Services
             }
 
             return credential;
+        }
+
+        private string GetAccessToken(string relatedUser)
+        {
+            string[] scopes = {
+            };
+
+            string[] scopesGC = {
+                ClassroomService.Scope.ClassroomCourses,
+                ClassroomService.Scope.ClassroomRosters,
+                ClassroomService.Scope.ClassroomProfileEmails
+            };
+
+            var dataRequest = new GenerateAccessTokenRequest();
+            dataRequest.Scope = scopesGC;
+            dataRequest.Lifetime = "3600s";
+            UserCredential credential;
+            using (var stream =
+                    new FileStream("credentials.json", FileMode.Open, FileAccess.Read))
+            {
+                string credPath = "token.json";
+                credential = GoogleWebAuthorizationBroker.AuthorizeAsync(
+                    GoogleClientSecrets.FromStream(stream).Secrets,
+                    scopes,
+                    relatedUser,
+                    CancellationToken.None,
+                    new FileDataStore(credPath, true)).Result;
+            }
+            IClientService clientService = new IAMCredentialsService();
+
+            ServiceAccountsResource.GenerateAccessTokenRequest request = new ServiceAccountsResource.GenerateAccessTokenRequest(
+                clientService,
+                dataRequest,
+                "projects/-/serviceAccounts/sa-classroom-admin@hits-classroom-1661148456378.iam.gserviceaccount.com"
+            );
+            request.Credential = credential;
+
+            var response = request.Execute();
+            Debug.WriteLine(response.AccessToken);
+
+            return response.AccessToken;
         }
     }
 }
